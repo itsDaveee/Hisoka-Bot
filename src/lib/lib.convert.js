d3c773e64dd2d132dcb5907342f2a33ad1e5580b
itsDaveee <glaisildavesonmessi@gmail.com>
2026-02-21 17:34:29 -0500

    FIX: Forcing new import syntax for cheerio

--- src/lib/lib.convert.js
+++ src/lib/lib.convert.js
@@ -1,4 +1,6 @@
-import * as cheerio from "cheerio"
-import axios from "axios"
-import { exec } from "child_process"
-import fs from "fs"
+import * as cheerio from 'cheerio';
+import axios from 'axios';
+import { exec } from 'child_process';
+import fs from 'fs';
+
+// Le reste du code suit...
